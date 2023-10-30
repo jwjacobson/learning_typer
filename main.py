2f42d062efafabc8c7b1a92f0d2ba5ff10754dc1
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-10-30 15:02:24 -0400

    Add formal option with differentiated responses

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import typer
 
-def main(firstname: str, lastname: str):
-    print(f"Hello {firstname} {lastname}")
+def main(firstname: str, lastname: str, formal: bool=False):
+    if formal:
+        print(f'Hello, {firstname.title()} {lastname.title()}')
+    else:
+        print(f'hey {firstname.lower()}')
 
 if __name__ == "__main__":
     typer.run(main)
