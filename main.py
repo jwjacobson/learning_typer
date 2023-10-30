25b8340ca572eb2e8eb6e326dc16c391b76adb11
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-10-30 15:03:56 -0400

    Change lastname argument into option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import typer
 
-def main(firstname: str, lastname: str, formal: bool=False):
+def main(firstname: str, lastname: str="", formal: bool=False):
     if formal:
         print(f'Hello, {firstname.title()} {lastname.title()}')
     else:
