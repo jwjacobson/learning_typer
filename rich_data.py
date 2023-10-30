31bbac388f1f43b8814d6de0775dcc5ade687f00
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-10-30 17:23:42 -0400

    Change default table and print different colors and styles

--- rich_data.py
+++ rich_data.py
@@ -4,17 +4,33 @@ import typer
 
 from rich.console import Console
 from rich.table import Table
+from rich import print
+from rich .pretty import pprint
 
 console = Console()
 
 
-def main():
-    table = Table("Name", "Item")
-    table.add_row("Rick", "Portal Gun")
-    table.add_row("Morty", "Plumbus")
+def table():
+    #prints a table
+    pprint('A Table:')
+    table = Table("Name", "Instrument")
+    table.add_row("Trane", "Tenor")
+    table.add_row("Wynton", "Piano")
     console.print(table)
+    print('\n')
 
+def colors():
+    pprint('Colors and other styling:')
+    print('[red]red[/red]')
+    print('[italic yellow]italic yellow[/italic yellow]')
+    print('[bold magenta]bold magenta[/bold magenta]')
+    print('[bold magenta on cyan]plus cyan background[/bold magenta on cyan]')
+    print('[link=https://www.soundcloud.com/solanaasu]link to my scloud[/link]')
+    print("emojis: :face_with_open_mouth-emoji: :person_surfing: (list with python -m rich.emoji)")
 
+def main():
+    table()
+    colors()
 
 if __name__ == "__main__":
     typer.run(main)
